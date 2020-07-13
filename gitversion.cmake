410e383d4366ca0e6c5a0c9e25350229e97acd81
Conor McCarthy <cmccarthy1@kx.com>
2020-07-13 20:24:26 +0100

    update to include cached repo changes

--- gitversion.cmake
+++ gitversion.cmake
@@ -3,11 +3,11 @@
 # or more generally as part of the travis build process
 
 execute_process(COMMAND git log --pretty=format:'%h' -n 1
-                OUTPUT_VARIABLE GIT_REV
+                OUTPUT_VARIABLE GIT_REF
                 ERROR_QUIET)
 
-if ("${GIT_REV}" STREQUAL "")
-  set(GIT_REV "N/A")
+if ("${GIT_REF}" STREQUAL "")
+  set(GIT_REF "N/A")
   set(GIT_DIFF "N/A")
   set(GIT_TAG "N/A")
   set(GIT_BRANCH "N/A")
@@ -23,35 +23,30 @@ else()
   
   # Windows seems to have issues with the use of the conditional echo used (alternative potentially needed)
   if(WIN32)
-    set(GIT_DIFF "N/A")
+    set(GIT_DIFF "nyi Windows")
   else()  
     execute_process(
-      COMMAND bash -c "git diff --quiet --exit-code || echo diff-present"
+      COMMAND bash -c "git diff --quiet --exit-code || echo 1b"
       OUTPUT_VARIABLE GIT_DIFF ERROR_QUIET)
+    execute_process(
+      COMMAND bash -c "git diff --quiet --cached --exit-code || echo 1b"
+      OUTPUT_VARIABLE GIT_DIFF_CACHED ERROR_QUIET)
   endif()
 
-  if(WIN32)
-    set(OSFLAG "Windows")
-  elseif(APPLE)
-    set(OSFLAG "MacOS")
-  else()
-    set(OSFLAG "Linux")
-  endif()
-
-  string(STRIP "${OSFLAG}" OSFLAG)
+  string(STRIP "${GIT_DIFF_CACHED}" GIT_DIFF_CACHED)
   string(STRIP "${GIT_DIFF}" GIT_DIFF)
-  string(STRIP "${GIT_REV}" GIT_REV)
-  string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
+  string(STRIP "${GIT_REF}" GIT_REF)
+  string(SUBSTRING "${GIT_REF}" 1 7 GIT_REF)
   string(STRIP "${GIT_TAG}" GIT_TAG)
   string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
 endif()
 
 # Set the contents to be written to version.txt
-set(VERSION "GIT_REV=${GIT_REV}
-GIT_TAG=${GIT_TAG}
-GIT_BRANCH=${GIT_BRANCH}
-GIT_DIFF=${GIT_DIFF}
-OS_INFO=${OSFLAG}")
+set(VERSION "GitRef=${GIT_REF}
+GitTag=${GIT_TAG}
+GitBranch=${GIT_BRANCH}
+GitDiff=${GIT_DIFF}
+GitDiffCached=${GIT_DIFF_CACHED}")
 
 # Check if a version.txt file exists and retrieve its contents
 if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/q/version.txt)
